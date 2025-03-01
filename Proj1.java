78054cf6baf8e121147280a3c8e47772a988c5e5
Kian Drees <kdrees@sandiego.edu>
2025-03-01 10:45:26 -0800

    writign stufff

--- Proj1.java
+++ Proj1.java
@@ -1,7 +1,15 @@
 public class Proj1 {
     public static void main(String[] args){
-        //first read the file
-        //do the algorithm
-        //print the output
+        System.out.println("Welcome to our first neural network - A Perceptron Net!");
+        System.out.println("1) Enter 1 to train the net on a data file");
+
+    }
+
+    public void train(){
+
+    }
+
+    public void test(){
+
     }
 }
