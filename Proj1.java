67e9b7087372dfaf250e42b50bd6e91871e4647b
Precee <pginigeme@sandiego.edu>
2025-03-01 11:55:05 -0800

    Added activation function and calculate yin helper function

--- Proj1.java
+++ Proj1.java
@@ -1,7 +1,32 @@
 public class Proj1 {
+
     public static void main(String[] args){
         //first read the file
         //do the algorithm
         //print the output
+
+    }
+
+    double calculateY_in( double [] weights, int[] inputs) {
+        double linearCombination = 0;
+        //Add the bias
+        linearCombination+= inputs[0];
+        for(int idx = 1; idx < inputs.length; idx++){
+            double product = (inputs[idx] * weights[idx]);
+            linearCombination += product;
+        }
+        return linearCombination;
+    }
+
+    int classifyLinearCombination(double linearCombination, double threshold){
+        if(linearCombination > threshold){
+            return 1;
+        }
+        else if(linearCombination < (-1 * threshold)){
+            return -1;
+        }
+        else{
+            return 0;
+        }
     }
 }
